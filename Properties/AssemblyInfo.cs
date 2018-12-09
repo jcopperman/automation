03cb18ac23141faeecd2e9495825b7b7bfbde23f
Jonathan Opperman <jopperman@entersekt.com>
2018-12-09 13:39:53 +0200

    Initial commit

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AutomationAssessment")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Entersekt Technologies (Pty) Ltd")]
+[assembly: AssemblyProduct("AutomationAssessment")]
+[assembly: AssemblyCopyright("Copyright © Entersekt Technologies (Pty) Ltd 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4f6c5a41-cb48-4949-a1eb-fc7dd472525c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
