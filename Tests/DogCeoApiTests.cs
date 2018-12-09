bd0a30ce9e2de1ba3d69e0f34fadf9d9edc36a94
Jonathan Opperman <jopperman@entersekt.com>
2018-12-09 14:29:14 +0200

    Added query_paramaters.json and skeleton tests for dog ceo api

--- Tests/DogCeoApiTests.cs
+++ Tests/DogCeoApiTests.cs
@@ -4,11 +4,24 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace AutomationAssessment
 {
     [TestClass]
-    public class UnitTest1
+    public class DogCeoApiTests 
     {
         [TestMethod]
-        public void TestMethod1()
+        public void VerifyStatusCodeIs200ForListAllDogBreeds()
         {
+            
+        }
+
+        [TestMethod]
+        public void VerifyBreedQueryIsInAllDogBreeds()
+        {
+
+        }
+
+        [TestMethod]
+        public void VerifyQueryIsInSubBreeds()
+        {
+
         }
     }
 }
